5e5c8830781df1b5655f70a3ad9591e0a582b1cf
giulia <giulia.cagnes@gmail.com>
2018-05-22 07:47:52 +0100

    aggiunta documentazione

--- src/it/unige/se/battaglianavale/Nave.java
+++ src/it/unige/se/battaglianavale/Nave.java
@@ -1,14 +1,42 @@
+/*
+ * Giulia Cagnes
+ * 
+ *  Version 0.1 (beta)
+ */
 package it.unige.se.battaglianavale;
 
 import java.util.ArrayList;
-
+/**
+ * Classe Nave 
+ * 
+ * @version          0.1 21 May 2018
+ * @author           Giulia Cagnes
+ *
+ */
 public class Nave {
+	/** Numero di navi*/
 	private static int numeroNavi = 0;
+	
+	/** Lunghezza delle navi */
 	private final int lunghezza;
+	
+	/** identificativo nave */
 	private final int id;
+	
+	/**parametro booleano indica se la nave e' affondata */
 	private boolean affondata;
+	
+	/**ArrayList di celle occupate*/
 	private ArrayList<Cella> celleOccupate;
 	
+	/**
+	 * Costruttore di nave
+	 * 
+	 * @param lunghezza della nave
+	 * @param id numero di navi
+	 * @param affondata booleano se lo è true se non lo è false
+	 *  
+	 */
 	public Nave(int lunghezza) {
 		this.lunghezza = lunghezza;
 		this.id = numeroNavi++;
@@ -17,6 +45,12 @@ public class Nave {
 		celleOccupate = new ArrayList<>();
 	}
 	
+	/**
+	 * Controllo se la nave è affondata se tutte le celle occupate
+	 * sono state colpite
+	 * 
+	 * @return boolean true se la nave e' affondata false altrimenti
+	 */
 	public boolean eAffondata() {
 		if(!affondata) {	
 			for (int i = 0; i < celleOccupate.size(); i++) {
@@ -28,15 +62,30 @@ public class Nave {
 		}
 		return affondata;
 	}
-
+	
+	/**
+	 * Gets Id.
+	 *
+	 * @return Id
+	 */
 	public int getId() {
 		return id;
 	}
-
+	
+	/**
+	 * Gets Lunghezza.
+	 *
+	 * @return Lunghezza
+	 */
 	public int getLunghezza() {
 		return lunghezza;
 	}
 	
+	/**
+	 * Aggiungo la cella da occupare all'arraylist delle celleOccupate
+	 *
+	 * @param cella da occupare
+	 */
 	public void occupaCella(Cella cellaDaOccupare) {
 		celleOccupate.add(cellaDaOccupare);
 	}
