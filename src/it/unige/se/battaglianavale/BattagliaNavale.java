2d6cfd9bae97198f866bdc671d3a4c7469e69e37
giulia <giulia.cagnes@gmail.com>
2018-06-04 12:10:18 +0200

    fix codice, aggiunti metodi come abbandona partita, convalida email e read file diventa carica regolamento da file

--- src/it/unige/se/battaglianavale/BattagliaNavale.java
+++ src/it/unige/se/battaglianavale/BattagliaNavale.java
@@ -13,8 +13,6 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import it.unige.se.battaglianavale.Utils.Direzione;
-
 /**
  * Classe Battaglia Navale contenente il main utile a mostrare tutte le funzionalità.
  * 
@@ -24,74 +22,48 @@ import it.unige.se.battaglianavale.Utils.Direzione;
  */
 
 public class BattagliaNavale {
-	/** ArrayList contiene utenti*/
-	private static ArrayList<UtenteRegistrato> utenti = new ArrayList<UtenteRegistrato>();
-	
-	/**ArrayList conitene utenti loggati, associazione fra battaglia navale e utente registrato*/
+	/* ArrayList contiene utenti */
+	private static ArrayList<UtenteRegistrato> utentiRegistrati = new ArrayList<UtenteRegistrato>();
+	/* ArrayList contiene utenti loggati, associazione fra battaglia navale e utente registrato */
 	private static ArrayList<UtenteRegistrato> utentiLoggati = new ArrayList<UtenteRegistrato>();
 
-	
+    private static String percorsoRegolamento = "src/Regole-del-gioco";
+
 	/**
 	 * Main function 
 	 * 
 	 * @param args argomenti
 	 */
 	public static void main(String[] args) {
-		utenti.add(new UtenteRegistrato("Giulia","Cagnes","giulia.cagnes@gmail.com","12345")); // aggiungo due utenti registrati
-		utenti.add(new UtenteRegistrato("Roberto","Bianchi","roberto.bianchi@gmail.com","54321"));
-		
-		String si = null;
-		Pattern pat = null;
-		Matcher mi = null;
+		String s = null;
+		Pattern pattern = null;
+		Matcher matcher = null;
 		
 		
 		Scanner input = new Scanner(System.in);
-		consoleLogin(input);// login degli utenti
 		
-		do {
-			System.out.println("inserisci: -> '1' per leggere le regole del gioco o -> '2' per iniziare la partita");
-			si = input.next();
-	        
-			pat = Pattern.compile("[1|2]");
-			mi = pat.matcher(si);
-		} while(!mi.matches());
+		// Aggiungo due utenti registrati per simulare persistenza
+		registraUtente(new UtenteRegistrato("Giulia","Cagnes","giulia.cagnes@gmail.com","12345"));
+		registraUtente(new UtenteRegistrato("Roberto","Bianchi","roberto.bianchi@gmail.com","54321"));
 		
-		if(si.equals("1")){
-			readFile();
-		}
-		
-		System.out.println("\n"+"In attesa del secondo giocatore..");
-		consoleLogin(input);
+		schermataLogin(input);
 
-		do {
-			System.out.println("inserisci: -> '1' per leggere le regole del gioco o -> '2' per iniziare la partita");
-			si = input.next();
-	        
-			pat = Pattern.compile("[1|2]");
-			mi = pat.matcher(si);
-		} while(!mi.matches());
 		
-		if(si.equals("1")){
-			readFile();
-		} 
-        boolean posizionaRandom = false;
-	
-	
 		
+		schermataPrincipale(input);
 		
 		do {
-			System.out.println("Inserisci lunghezze delle navi: ");
-			System.out.println("(es: [2,3,4] per avere 3 navi di dimensione 2,3 e 4 rispettivamente.)");
-		    System.out.println("Nota: lunghezza massima nave e' 5 e minima 1, puoi insierire da 1 fino a massimo 5 navi in campo.");
-			si = input.next();
+			System.out.println("Inserisci lunghezze delle navi (es: [2,3,4] per avere 3 navi di dimensione 2,3 e 4 rispettivamente): ");
+		    System.out.println("NB: lunghezza massima della nave e' 5 puoi insiririefino ad un massimo 5 navi in campo.");
+			s = input.next();
 
-			pat = Pattern.compile("^([1|2|3|4|5],){0,4}([1|2|3|4|5])$");
-			mi = pat.matcher(si);
-		} while(!mi.matches());
+			pattern = Pattern.compile("^([1|2|3|4|5],){0,4}([1|2|3|4|5])$");
+			matcher = pattern.matcher(s);
+		} while(!matcher.matches());
 		
 		
 		// trasforma la string in input in un array di stringhe (usando la virgola come separatore) es. 3,4,2 -> [3 4 2]
-		String[] lunghezzaNaviStr = si.split(",");
+		String[] lunghezzaNaviStr = s.split(",");
 		
 		// istanzio l'array lunghezza navi la cui dimensione e la stessa del vettore sopra
 		int[] lunghezzaNavi = new int[lunghezzaNaviStr.length];
@@ -106,236 +78,227 @@ public class BattagliaNavale {
 		Giocatore giocatoreA = new Giocatore(utentiLoggati.get(0).getName(), lunghezzaNavi);
 		Giocatore giocatoreB = new Giocatore(utentiLoggati.get(1).getName(), lunghezzaNavi);
 
-		Partita p = new Partita(giocatoreA,giocatoreB);
-		
-		Giocatore diTurno = p.getDiTurno();
-//		int i=0;
-		
-//		for(String lunghezzaNave:si.split(",")){
-//			lunghezzaNavi[i]=Integer.parseInt(lunghezzaNave);
-//			System.out.println(lunghezzaNavi[i]);
-	//		i++;
-//		}
+		Partita partita = new Partita(giocatoreA,giocatoreB);
 
+		// Fase di posizionamento navi			
 		do {
-			System.out.println("inserisci: -> 1 se vuoi scacchiera navi random ");
-			System.out.println("           -> 2 se vuoi posizionare navi manualmente");
-			si = input.next();
+			System.out.println("Premere:");
+			System.out.println("[R]andom: per disporre le navi randomiament");
+			System.out.println("[M]anuale: per disporre le navi manualmente");
+			s = input.next();
 	        
-			pat = Pattern.compile("[1|2]");
-			mi = pat.matcher(si);
-		} while(!mi.matches());
+			pattern = Pattern.compile("[R|M]", Pattern.CASE_INSENSITIVE);
+			matcher = pattern.matcher(s);
+		} while(!matcher.matches());
 		
-		if(si.equals("1")){
-			posizionaRandom = true;
+		if(s.toLowerCase().equals("r")){
+			partita.posizionaNaviRandom();
 		} else {
-			posizionaRandom = false;
-		}
-		// Posiziona Navi			
-		if(posizionaRandom) {
-				p.posizionaNaviRandom();		
-		} else {
-			
-			do {
-				System.out.println(diTurno);
-				System.out.println(diTurno.getCampo());
-
-				System.out.println("Posizionamento Navi...");
-				System.out.println("inserisci delle coordinate e una direzione es. [1,3,U] :");
-				System.out.println("[#riga, #colonna, direzione possibile: U_= UP , D = DOWN, L = LEFT, R = RIGHT]");
-			
-				String s = input.next();
-	        
-				Pattern pattern = Pattern.compile("(\\d),(\\d),([U|D|L|R])");
-				Matcher m = pattern.matcher(s);
-			
-					if (m.matches()) {
-						int y = Integer.parseInt(m.group(1));
-						int x = Integer.parseInt(m.group(2));
-						String dir = m.group(3);
-				
-						switch (dir) {
-						case "U":
-							diTurno.posizionaNave(x, y, Direzione.SU);
-							break;
-						case "D":
-							diTurno.posizionaNave(x, y, Direzione.GIU);
-							break;
-						case "L":
-							diTurno.posizionaNave(x, y, Direzione.SINISTRA);
-							break;
-						case "R":
-							diTurno.posizionaNave(x, y, Direzione.DESTRA);
-							break;
-						}
-				
-					} if (!diTurno.haNaviDaPosizionare()) {
-						diTurno = p.cambiaTurno();
-					}
-				} while (diTurno.haNaviDaPosizionare());
+			partita.posizionaNaviManualmente(input);
 		}
 		
 		// Fase di gioco
-		while(!p.eTerminata()) {
+		while(!partita.eTerminata()) {
 			boolean haColpito = true;
 
-			System.out.println(p);
+			System.out.println(partita);
 			System.out.println("Spara un colpo!");
 			System.out.println("inserisci delle coordinate [1,3] :");
 			System.out.println("[#riga,#colonna]");
 			
-	        String s = input.next();
+	        s = input.next();
 	        
-			Pattern pattern = Pattern.compile("(\\d),(\\d)");
-			Matcher m = pattern.matcher(s);
+			pattern = Pattern.compile("(\\d),(\\d)");
+			matcher = pattern.matcher(s);
 			
-			if (m.matches()) {
-				int y = Integer.parseInt(m.group(1));
-				int x = Integer.parseInt(m.group(2));
+			if (matcher.matches()) {
+				int y = Integer.parseInt(matcher.group(1));
+				int x = Integer.parseInt(matcher.group(2));
 			
-				haColpito = p.attacca(x, y);
-			}
-				
+				haColpito = partita.attacca(x, y);
+			}	
 			
 			if(!haColpito) {
-				p.cambiaTurno();	
+				partita.cambiaTurno();	
 			}
-			
 		}
 		input.close();
+		
+		System.exit(0);
 	}
 	
-	public static void consoleLogin(Scanner input) {
+	private static void schermataLogin(Scanner input) {
+		do {
+			String sceltaUtente;
+			
+			Matcher matcher;
+			do {
+				System.out.println(">>>>> Benvenuto nel gioco di Battaglia Navale online! <<<<<");				
+				System.out.println("Premere:");
+				System.out.println("[R]egistra un nuovo utente");
+				System.out.println("[L]og in");
+				sceltaUtente = input.next();
+		        
+				Pattern pattern = Pattern.compile("[R|L]", Pattern.CASE_INSENSITIVE);
+				matcher = pattern.matcher(sceltaUtente);
+			} while(!matcher.matches());
+			
+	
+			if(sceltaUtente.toLowerCase().equals("r")) {
+				registraUtenteDaConsole(input);
+				loginUtenteDaConsole(input);
+			} else { 
+				System.out.println("Login primo giocatore:");
+				loginUtenteDaConsole(input);
+				do {
+								
+					System.out.println("Secondo giocatore...Premere:");
+					System.out.println("[R]egistra un nuovo utente");
+					System.out.println("[L]og in");
+					sceltaUtente = input.next();
+			        
+					Pattern pattern = Pattern.compile("[R|L]", Pattern.CASE_INSENSITIVE);
+					matcher = pattern.matcher(sceltaUtente);
+				} while(!matcher.matches());
+				if(sceltaUtente.toLowerCase().equals("r")) {
+					registraUtenteDaConsole(input);
+					loginUtenteDaConsole(input);
+				
+				}else {
+				System.out.println("login secondo giocatore");
+				loginUtenteDaConsole(input);		
+				}
+			}
+		}while(utentiLoggati.size() < 2);
+	}
+
+	public static void loginUtenteDaConsole(Scanner input) {
 		String email = null;
 		String password = null;
 		
 		UtenteRegistrato u = null;
 		do {
-			System.out.println("inserisci email:");
+			System.out.println("email:");
 			
-			String s = input.next();
-			
-			/* https://stackoverflow.com/questions/8204680/java-regex-email */
-		 	Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-		 	
-			Matcher m = pattern.matcher(s);
-			
-			if (!m.matches()) {
-			
-				System.out.println("Inserire nuovamente un email di un formato corretto [es. pippo123@gmail.com]");
-				continue;
-			}
-			email = s;
-			
-			System.out.println("Inserisci password:");
-			
+			email = convalidaEmail(input);
+				
+			System.out.println("password:");			
 			password = input.next();
 			
-			u = login(email, password);
+			u = verificaLogin(email,password);
 		}while(u == null);
 		
 		utentiLoggati.add(u);
 	}
 	
-	public static UtenteRegistrato login(String email, String password) {
+	private static String convalidaEmail(Scanner input) {
+		Pattern pattern = null;
+		Matcher matcher = null;
 		
+		String email = null;
+		do {
+			System.out.println("inserisci email di un formato corretto (es. pippo123@gmail.com):");
+			email = input.next();
+			
+			// https://stackoverflow.com/questions/8204680/java-regex-email 
+			pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+			matcher = pattern.matcher(email);
+		}while(!matcher.matches());
 		
-		String si = null;
-		Pattern pat = null;
-		Matcher mi = null;
-		
-		
-		Scanner input = new Scanner(System.in);
-		for(int i=0;i<utenti.size();++i) {
-			UtenteRegistrato corrente = utenti.get(i);
+		return email;
+	}
+
+	/*
+	 * Verifica che credenziali siano registrate nel sistema.
+	 * @param email
+	 * @param password
+	 * @return L'utente registrato in caso positivo, null altrimenti
+	 * */
+	public static UtenteRegistrato verificaLogin(String email, String password) {	
+		for(int i=0;i<utentiRegistrati.size();++i) {
+			UtenteRegistrato corrente = utentiRegistrati.get(i);
 			if((corrente.getEmail().equals(email)) && (corrente.getPassword().equals(password))){
 				System.out.println("Utente registrato");
 				return corrente;
 			}
 		}
-		System.out.println("Utente non registrato");
-		do {
-			System.out.println("Vuoi effettuare registrazione utente?  -> 1 Registrati  -> 2 Reinserisci credenziali corrette ");
-			si = input.next();
-	        
-			pat = Pattern.compile("[1|2]");
-			mi = pat.matcher(si);
-		} while(!mi.matches());
-		
-		if(si.equals("1")){
-			registrazioneUtente();
-		}if (si.equals("2")) {
-			consoleLogin(input);
-		}
 		
 		return null;
 	}
 	
-	public static void readFile() {
-	     
-	    String path = "src/Regole-del-gioco";
+	public static String caricaRegolamentoDaFile() {
 	    char[] in = new char[500000];
 	    int size = 0;
+	    String regolamento="";
+	    
 	    try {
-	        File file = new File(path);
+	        File file = new File(percorsoRegolamento);
 	        
 	        FileReader fr = new FileReader(file);
 	        size = fr.read(in);
-	         
-	      
-	      
-	         
-	        for(int i=0; i<size; i++)
-	        System.out.print(in[i]);
-	        System.out.println("\n");
+	        for(int i=0; i<size; i++) {
+	        	regolamento+=in[i];
+	        }
+	        regolamento+="\n";
 	        fr.close();
-	         
 	    } catch(IOException e) { 
 	        e.printStackTrace();
 	    }
+	    
+	    return regolamento;
 	}
 	
-	public static void registrazioneUtente() {
+	public static void registraUtenteDaConsole(Scanner input) {		
+		System.out.println("Inserisci Nome:");
+		String nome = input.next();
 		
-		String email = null;
-		String password = null;
+		System.out.println("Inserisci Cognome:");
+		String cognome = input.next();
+	
+		System.out.println("Inserisci Email:");
+		String email = convalidaEmail(input);
 		
-		Scanner input = new Scanner(System.in);
+		System.out.println("Inserisci password:");		
+		String password = input.next();
 		
-			System.out.println("Inserisci Nome:");
-			String n = input.next();
-			
-			System.out.println("Inserisci Cognome:");
-			String c = input.next();
-			
-		
-			System.out.println("Inserisci Email:");
-			String s = input.next();
-			
-			/* https://stackoverflow.com/questions/8204680/java-regex-email */
-		 	Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-		 	
-			Matcher m = pattern.matcher(s);
-			
-			if (!m.matches()) {
-			
-				System.out.println("Inserire nuovamente un email di un formato corretto [es. pippo123@gmail.com]");
-				
-			}
-			email = s;
-			
-			System.out.println("Inserisci password:");
-			
-			password = input.next();
-			
-			System.out.println("Registrazione utente effettuata correttamente! Ora effettua il Login:");
-		  
-			utenti.add(new UtenteRegistrato(n,c,email,password));
-			
-		
+		utentiRegistrati.add(new UtenteRegistrato(nome,cognome,email,password));
+	}
+	
+	public static void registraUtente(UtenteRegistrato utente) {
+		utentiRegistrati.add(utente);
 	}
 		
+	private static void schermataPrincipale(Scanner input) {
+		Matcher matcher = null;
+		String s = null;
 		
+		do {
+			System.out.println("Premere:");
+			System.out.println("[I]nizia: per giocare direttamente la partita");
+			System.out.println("[R]egolamento: per consultare il regolamento prima di iniziare la partita");
+			s = input.next();
+	        
+			Pattern pattern = Pattern.compile("[R|I]", Pattern.CASE_INSENSITIVE);
+			matcher = pattern.matcher(s);
+			
+			if(s.toLowerCase().equals("r")) {
+				consultaRegolamento(input);
+			}
+		} while(!matcher.matches());
+	}
+
+	public static void consultaRegolamento(Scanner input) {
+		// stampa regolamento
+		System.out.println(caricaRegolamentoDaFile());
+		
+		System.out.println("Premere un tasto per iniziare la partita");
+		input.next();
+	}
+	
+	public static void abbandonaPartita() {
+		System.exit(1);
+	}
+	
 		
 	}
 
