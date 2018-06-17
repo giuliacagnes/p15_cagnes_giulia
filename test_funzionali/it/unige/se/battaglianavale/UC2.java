591f47ce85a0f534ef6610dbd984488222335997
giulia <giulia.cagnes@gmail.com>
2018-06-17 15:01:01 +0200

    fix codice

--- test_funzionali/it/unige/se/battaglianavale/UC2.java
+++ test_funzionali/it/unige/se/battaglianavale/UC2.java
@@ -4,23 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-//verifica login
-public class UC1 {
+
+public class UC2 {
 
 	@Before
-	public void init() {
+	public void setUp() throws Exception {
 		BattagliaNavale.registraUtente(new UtenteRegistrato("Test", "SuperTest", "test@gmail.com", "123"));
-	}	
-	
-	@Test
-	public void testLoginFailed() {
-		assertNull("l'utente si è loggato con password errata",BattagliaNavale.verificaLogin("test@gmail.com","1234"));
-		assertNull("l'utente si è loggato con email errata",BattagliaNavale.verificaLogin("test2@gmail.com","123"));
 	}
-	
+
+	@Test
+		public void testLoginFailed() {
+			assertNull("l'utente si è loggato con password errata",BattagliaNavale.verificaLogin("test@gmail.com","1234"));
+			assertNull("l'utente si è loggato con email errata",BattagliaNavale.verificaLogin("test2@gmail.com","123"));
+		}
 	@Test
 	public void testLoginSuccessfull() {
 		assertNotNull("",BattagliaNavale.verificaLogin("test@gmail.com","123"));
 	}
+
 
 }
