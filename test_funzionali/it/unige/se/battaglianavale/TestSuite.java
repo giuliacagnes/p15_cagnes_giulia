4e7fa0e774816fb1384c1f8f49061ff45f256c66
giulia <giulia.cagnes@gmail.com>
2018-06-17 15:04:09 +0200

    aggiunto TestSuite

---
+++ test_funzionali/it/unige/se/battaglianavale/TestSuite.java
@@ -0,0 +1,11 @@
+package it.unige.se.battaglianavale;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UC10.class, UC11.class, UC2.class, UC5.class})
+public class TestSuite {
+
+}
